5d9e0ad5e63fec002e7587cf07f51bf8ab9030df
xingxingmofashu <xingxingmofashu@outlook.com>
2024-09-18 15:44:32 +0800

    chore: rename

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import { resolve } from "node:path"
 import dayjs from "dayjs"
 import { defineConfig } from "vite"
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/neplayer/index.ts"),
+        name: "neplayer",
+      },
+    },
     define: {
       __BUILD_TIME__: JSON.stringify(dayjs().format("YYYY/MM/DD HH:mm")),
     },
