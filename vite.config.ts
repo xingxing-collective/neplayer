12e57cff148c14cd39d1926c75e779b3a75a0363
星星魔法术 <xingxingmofashu@outlook.com>
2024-10-15 16:51:29 +0800

    feat(playground): playground (#19)
    * feat(playground): playground
    
    * fix:Cannot install with "frozen-lockfile"

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,11 @@ import alias from "@rollup/plugin-alias"
 import resolve from "@rollup/plugin-node-resolve"
 import Vue from "@vitejs/plugin-vue"
 import tailwindcss from "tailwindcss"
-import IconsResolver from "unplugin-icons/resolver"
 import Icons from "unplugin-icons/vite"
-import Components from "unplugin-vue-components/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-    Icons(),
-    Components({
-      resolvers: [IconsResolver()],
-    }),
-  ],
+  plugins: [Vue(), Icons()],
   build: {
     sourcemap: true,
     lib: {
