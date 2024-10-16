b2e2802d31a2169d78740c423e1dd60aaaf6d112
星星魔法术 <xingxingmofashu@outlook.com>
2024-10-16 15:52:35 +0800

    Update release.sh (#34)

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,22 +11,9 @@ pnpm build
 # use absolute urls for better rendering on npm
 sed -i.bak 's/\.\/\.github\/assets/https:\/\/github.com\/xingxing-collective\/neplayer\/tree\/main\/\.github\/assets/g' README.md
 
+mv README.md.bak README.md
+
 # Release packages
 TAG="latest"
 
 pnpm publish --access public --no-git-checks --tag $TAG
-
-
-# Restore README.md from backup
-if [ -f README.md.bak ]; then
-    mv README.md.bak README.md
-    echo "Restored README.md from backup."
-else
-    echo "No backup found for README.md."
-fi
-
-# Optionally remove the .bak file
-if [ -f README.md.bak ]; then
-    rm README.md.bak
-    echo "Removed backup file README.md.bak."
-fi
